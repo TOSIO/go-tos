44de39b89a25e511e53b7e142ccfddbdfc571bd4
duyu <514641931@qq.com>
2018-09-04 18:37:57 +0800

    modify common

--- devbase/common/types.go
+++ devbase/common/types.go
@@ -26,8 +26,8 @@ import (
 	"reflect"
 	"strings"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/tos/go-tos/crypto/sha3"
+	"github.com/tos/go-tos/devbase/common/hexutil"
 )
 
 // Lengths of hashes and addresses in bytes.
