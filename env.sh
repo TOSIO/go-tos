f0a6e8700b6d246f7d109b1952a44ba265103925
johncao <john@toschain.org>
2018-09-05 17:19:41 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -2,16 +2,4 @@
 
 export GOROOT=/usr/local/go
 export GOPATH=$PWD
-cd src/github.com/TOSIO
-
-echo "go get github.com/mattn/go-colorable"
-go get github.com/mattn/go-colorable
-echo "go get github.com/aristanetworks/goarista/monotime"
-go get github.com/aristanetworks/goarista/monotime
-echo "go get github.com/go-stack/stack"
-go get github.com/go-stack/stack
-
-echo "go get github.com/influxdata/influxdb/client"
-go get github.com/influxdata/influxdb/client
-echo "go get is over."
 
