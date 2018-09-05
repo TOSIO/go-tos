02f612002e6f6e8f42cf68f9d7140211e8f5fd85
duyu <514641931@qq.com>
2018-09-05 16:17:44 +0800

    Common partial package import path modification

--- devbase/common/bitutil/compress_test.go
+++ devbase/common/bitutil/compress_test.go
@@ -21,7 +21,7 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/TOSIO/go-tos/devbase/common/hexutil"
 )
 
 // Tests that data bitset encoding and decoding works and is bijective.
