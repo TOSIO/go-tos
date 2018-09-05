5ac8caa3b32b59ec4814e72004ecb1f2d0952b2a
johncao <john@toschain.org>
2018-09-05 10:31:04 +0800

    add .gitignore and update crypto for test

--- devbase/crypto/ecies/ecies_test.go
+++ devbase/crypto/ecies/ecies_test.go
@@ -40,7 +40,7 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/tos/go-tos/devbase/crypto"
 )
 
 var dumpEnc bool
--- devbase/crypto/signature_test.go
+++ devbase/crypto/signature_test.go
@@ -22,9 +22,9 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/common/hexutil"
-	"github.com/ethereum/go-ethereum/common/math"
+	"github.com/tos/go-tos/devbase/common"
+	"github.com/tos/go-tos/devbase/common/hexutil"
+	"github.com/tos/go-tos/devbase/common/math"
 )
 
 var (
