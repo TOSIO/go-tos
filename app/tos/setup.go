61c965ff15bbc3612cf40bccce723e088ba1dde3
slchi <chisonglin@toschain.org>
2018-09-13 09:39:12 +0800

    add tos main

---
+++ app/tos/setup.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	godebug "runtime/debug"
+
+	"github.com/TOSIO/go-tos/internal/debug"
+	"github.com/TOSIO/go-tos/app/utils"
+	"github.com/TOSIO/go-tos/devbase/log"
+	"github.com/TOSIO/go-tos/node"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+//读取配置文件、各模块应用命令行参数进行配置初始化
+//返回节点对象
+func makeConfigNode(ctx *cli.Context) (*node.Node, TOSConfig) {
+	// Load defaults.
+	cfg := TOSConfig{
+		// 各模块config变量初始化
+		//Dashboard: dashboard.DefaultConfig,
+
+	}
+
+	// Load config file.
+	if file := ctx.GlobalString(configFileFlag.Name); file != "" {
+		if err := loadConfig(file, &cfg); err != nil {
+			utils.Fatalf("%v", err)
+		}
+	}
+
+	// Apply flags.应用命令行传递进来的参数（参数封装在ctx中）
+	utils.SetNodeConfig(ctx, &cfg.Node)
+	stack, err := node.New(&cfg.Node)
+	if err != nil {
+		utils.Fatalf("Failed to create the protocol stack: %v", err)
+	}
+
+	// 其他模块config设置
+
+	return stack, cfg
+}
+
+func activePPROF(ctx *cli.Context)error {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	logdir := ""
+	if ctx.GlobalBool(utils.DashboardEnabledFlag.Name) {
+		logdir = (&node.Config{DataDir: utils.MakeDataDir(ctx)}).ResolvePath("logs")
+	}
+	if err := debug.Setup(ctx, logdir); err != nil {
+		return err
+	}
+	// Cap the cache allowance and tune the garbage collector
+	var mem gosigar.Mem
+	if err := mem.Get(); err == nil {
+		allowance := int(mem.Total / 1024 / 1024 / 3)
+		if cache := ctx.GlobalInt(utils.CacheFlag.Name); cache > allowance {
+			log.Warn("Sanitizing cache to Go's GC limits", "provided", cache, "updated", allowance)
+			ctx.GlobalSet(utils.CacheFlag.Name, strconv.Itoa(allowance))
+		}
+	}
+	// Ensure Go's GC ignores the database cache for trigger percentage
+	cache := ctx.GlobalInt(utils.CacheFlag.Name)
+	gogc := math.Max(20, math.Min(100, 100/(float64(cache)/1024)))
+
+	log.Debug("Sanitizing Go's GC trigger", "percent", int(gogc))
+	godebug.SetGCPercent(int(gogc))
+
+	// Start metrics export if enabled
+	//utils.SetupMetrics(ctx)
+
+	// Start system runtime metrics collection
+	go metrics.CollectProcessMetrics(3 * time.Second)
+
+	return nil
+}
+
+//生成节点对象、注册服务
+func makeFullNode(ctx *cli.Context) *node.Node {
+	stack, cfg := makeConfigNode(ctx)
+
+	// 服务注册
+	//utils.RegisterEthService(stack, &cfg.Eth)
+
+	//if ctx.GlobalBool(utils.DashboardEnabledFlag.Name) {
+	//	utils.RegisterDashboardService(stack, &cfg.Dashboard, gitCommit)
+	//}
+	return stack
+}
+
+//启动账户相关服务
+func activeAccount(ctx *cli.Context, stack *node.Node) {
+	
+}
+
+//启动辅助服务
+func startAuxservice(ctx *cli.Context, stack *node.Node){
+
+}
+
+//钱包循环
+func walletloop(stack *node.Node)
+{
+
+}
+
+// startNode boots up the system node and all registered protocols, after which
+// it unlocks any requested accounts, and starts the RPC/IPC interfaces and the
+// miner.
+// 启动节点（本机）
+func startNode(ctx *cli.Context, stack *node.Node) {
+	debug.Memsize.Add("node", stack)
+
+	// Start up the node itself
+	utils.StartNode(stack)
+
+	// 账户相关
+	activeAccount(ctx, stack)
+
+	// 钱包相关
+	go walletloop()
+
+	// 启动辅助服务（如挖矿、内存交易池等）
+	// Start auxiliary services if enabled
+	startAuxservice(ctx, stack)
+}
