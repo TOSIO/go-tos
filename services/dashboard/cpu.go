3bd767aea92d14d75246e7441523253788c0ab04
candy <zhouyating@toschain.org>
2018-10-20 14:13:54 +0800

    add cpu.go modified

--- services/dashboard/cpu.go
+++ services/dashboard/cpu.go
@@ -21,7 +21,7 @@ package dashboard
 import (
 	"syscall"
 
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/TOSIO/go-tos/devbase/log"
 )
 
 // getProcessCPUTime retrieves the process' CPU time since program startup.
