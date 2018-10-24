500055e74f1732365b4c6934300a93eda6111b83
wll <wanglongliang@toschain.org>
2018-10-24 09:39:37 +0800

    block diff compute

--- devbase/utils/pow.go
+++ devbase/utils/pow.go
@@ -3,12 +3,24 @@ package utils
 import (
 	"github.com/TOSIO/go-tos/devbase/common"
 	"math/big"
+	"fmt"
+	"github.com/TOSIO/go-tos/devbase/common/math"
 )
 
-func CalculateWork(hash common.Hash) *big.Int{
+func CalculateWork(hash common.Hash) *big.Int {
 	// 参考6.1 区块难度计算
+	//(2^128-1)/(hash_little / 2^160), hash_little长度为256 bits(32 bytes)
+	//1.little hash
+	little_hash := new(big.Int).SetBytes(hash[:])
 
+	//2.根据公式计算
+	//分母 (2^128-1)
+	den := new(big.Int).Sub(math.BigPow(2, 128), big.NewInt(1))
+	//分子 (hash_little / 2^160)
+	num := new(big.Int).Div(little_hash, math.BigPow(2, 160))
+	hd := new(big.Int).Div(den, num)
 
+	fmt.Println(hd)
 
-	return  big.NewInt(1000)
+	return hd
 }
