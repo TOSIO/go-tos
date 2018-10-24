7ee000a63b24c75301e89ee717ace3453815c110
duyu <514641931@qq.com>
2018-10-24 12:00:18 +0800

    ConfirmUserRewardRate is represented by an integer

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -10,10 +10,10 @@ const (
 	OneTos                     = 1e18 //1 tos = 1e18 tls
 	DefaultGasPrice            = 100
 	DefaultGasLimit            = 1 << 32
-	TransferTransactionGasUsed = 21000     // 21000 tls
-	MiningGasUsed              = 21000     // 21000 tls
-	InitialRewardMiner         = 50 * 1e18 //50 tos
-	HalfLifeRewardMiner        = 2000000   //Half down every 2,000,000 main blocks
-	ConfirmUserRewardRate      = 0.2
-	ConfirmMinerRewardRate     = 1 - ConfirmUserRewardRate
+	TransferTransactionGasUsed = 21000                       // 21000 tls
+	MiningGasUsed              = 21000                       // 21000 tls
+	InitialRewardMiner         = 50 * 1e18                   //50 tos
+	HalfLifeRewardMiner        = 2000000                     //Half down every 2,000,000 main blocks
+	ConfirmUserRewardRate      = 20                          //Need to divide by 100
+	ConfirmMinerRewardRate     = 100 - ConfirmUserRewardRate //Need to divide by 100
 )
