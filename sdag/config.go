0f5454e7fd81e53326d49815398fa443edc829dc
slchi <chisonglin@toschain.org>
2018-10-31 17:42:56 +0800

    add miner parameter def

--- sdag/config.go
+++ sdag/config.go
@@ -6,11 +6,13 @@ import "github.com/TOSIO/go-tos/devbase/common"
 type Config struct {
 	NetworkId       uint64 // Network ID to use for selecting peers to connect to
 	DatabaseCache   int
-	DatabaseHandles int `toml:"-"`
-	Tosbase      common.Address `toml:",omitempty"`
+	DatabaseHandles int            `toml:"-"`
+	Tosbase         common.Address `toml:",omitempty"`
+	Mining          bool
 }
 
 var DefaultConfig = Config{
 	NetworkId:     1,
 	DatabaseCache: 768,
+	Mining:        false,
 }
